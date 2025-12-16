003a7e52cf1b26a362f7ecec86599b2d551f81aa
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-16 19:09:12 +0530

    63839

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+
 
 # Create your models here.
+from django import models
+
+class MenuCategory(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+        
