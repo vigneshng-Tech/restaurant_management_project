711bdfa429133b32d99a98cb6cd7e8630bfa614d
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-18 22:38:45 +0530

    64082

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import response
+from rest_framework.permissions import ISAuthenticated
+from.models import Order
+from.serializers import OrderSerializer
+
+class OrderHistoryAPIView(APIView)
+    permissions_classes = [ISAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.filter(user=request.user)
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
