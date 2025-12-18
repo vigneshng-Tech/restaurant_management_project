6faa5fa5644db25a5eaea403cb7702ac67b4e968
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-18 21:02:04 +0530

    64084

---
+++ orders/utils.py
@@ -0,0 +1,13 @@
+import string
+import secrets
+from orders.models import Coupon 
+
+
+def generate_coupon_code(length=10):
+    characters = string.ascii_uppercase + string.digits
+
+
+    while True:
+        code = ''.join(secrets.choice(characters) for _ in range(length))
+        if not Coupon.objects.filter(code=code).exists():
+            return code
