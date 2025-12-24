b2b330b2e12a9b5be8f0fdd9f4329935e00a152f
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-24 14:00:37 +0530

    64571

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import ItemView, ItemSearchView
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+    path('items/search/', ItemSearchView.as_view(), name='item-search'),
 ]
