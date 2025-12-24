1b27e3da77be6a1f2b612f55f1a58a59f1d2b213
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-24 23:10:56 +0530

    64569

--- home/models.py
+++ home/models.py
@@ -17,3 +17,23 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class DailySpecial(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def get_random_special():
+        """
+        Returns one random DailySpecial from the database.
+        If no specials exist, returns None.
+        """
+
+        specials = DailySpecial.objects.all()
+        if not sprcials.exists():
+            return None
+        return specials.order_by('?').first()
+        
